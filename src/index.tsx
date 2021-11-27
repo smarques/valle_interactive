f7eb3aeaba18a58db58f587dcabe9d5ff16ea89a
sergio marchesini <sergiomarchesini@gmail.com>
2021-11-27 17:19:28 +0100

    preloader

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 
 import * as ReactDOM from "react-dom";
-import {useState}  from "react";
+import {useState, Fragment}  from "react";
 import {Player} from "liqvid";
 import Intro from "./Components/Intro/Intro" 
 import DebugBar from "./Components/DebugBar/DebugBar" 
@@ -24,6 +24,7 @@ import tecnico_grafica_e_comunicazione from './assets/QR/GraficaComunicazione.pn
 import grafica_multimediale from './assets/QR/AudiovisivoMultimediale.png';
 import tecnico_turistico from './assets/QR/EconomicoTurismo.png';
 import fotografia from './assets/QR/Fotografia.png';
+import { Lines } from 'react-preloaders';
 
 function Game() {
   const [ completed, setCompleted ] = useState(0);
@@ -72,49 +73,52 @@ function Game() {
   };
 
   return (
-    <Player script={script} controls={null}>
-      <DebugBar  lang={lang} completed={completed} soundControl={{toggleSoundTrack, soundPlaying}}  />
-      <PedalBar lang={lang}/>
-      <Intro lang={lang} toggleLang={toggleLang}/>
-      <Partenza />
-      <PauseAt time="q1" />
-      <Question lang={lang} number="1" updateFx={updateScore} playBeep={playBeep} />
-      <Semaforo2Semaforo fr="q1/after" to="q2"/>
-      <PauseAt time="q2" />
-      <Question lang={lang} number="2" updateFx={updateScore} playBeep={playBeep} /> 
-      <Semaforo2Semaforo fr="q2/after" to="q3"/>
-      <PauseAt time="q3" />
-      <Question lang={lang} number="3" updateFx={updateScore} playBeep={playBeep} />
-      <Semaforo2Semaforo fr="q3/after" to="q4"/>
-      <PauseAt time="q4" />
-      <Question lang={lang} number="4" updateFx={updateScore} playBeep={playBeep} />
-      <Semaforo2Semaforo fr="q4/after" to="q5"/>
-      <PauseAt time="q5" />
-      <Question lang={lang} number="5" updateFx={updateScore} playBeep={playBeep} />
-      <Semaforo2Semaforo fr="q5/after" to="q6"/>
-      <PauseAt time="q6" />
-      <Question lang={lang} number="6" updateFx={updateScore} playBeep={playBeep} />
-      <Semaforo2Semaforo fr="q6/after" to="q7"/>
-      <PauseAt time="q7" />
-      <Question lang={lang} number="7" updateFx={updateScore} playBeep={playBeep} />
-      <Semaforo2Semaforo fr="q7/after" to="q8"/>
-      <PauseAt time="q8" />
-      <Question lang={lang} number="8" updateFx={updateScore} playBeep={playBeep} />
-      <Semaforo2Semaforo fr="q8/after" to="q9"/>
-      <PauseAt time="q9" />
-      <Question lang={lang} number="9" updateFx={updateScore} playBeep={playBeep} />
-      <Semaforo2Semaforo fr="q9/after" to="q10"/>
-      <PauseAt time="q10" />
-      <Question lang={lang} number="10" updateFx={updateScore} playBeep={playBeep} />
-      <Arrivo/>
-      <PauseAt time="response" />
-      <Response lang={lang} score={score} updateFx={updateScore} qr={QRs} />
-      <Fail />
-      <PauseAt time="retry" />
-      <Retry lang={lang} updateFx={updateScore} />
-      <PauseAt time="credits" />
-      <Credits lang={lang} />
-    </Player>
+    <Fragment>
+      <Player script={script} controls={null}>
+        <DebugBar  lang={lang} completed={completed} soundControl={{toggleSoundTrack, soundPlaying}}  />
+        <PedalBar lang={lang}/>
+        <Intro lang={lang} toggleLang={toggleLang}/>
+        <Partenza />
+        <PauseAt time="q1" />
+        <Question lang={lang} number="1" updateFx={updateScore} playBeep={playBeep} />
+        <Semaforo2Semaforo fr="q1/after" to="q2"/>
+        <PauseAt time="q2" />
+        <Question lang={lang} number="2" updateFx={updateScore} playBeep={playBeep} /> 
+        <Semaforo2Semaforo fr="q2/after" to="q3"/>
+        <PauseAt time="q3" />
+        <Question lang={lang} number="3" updateFx={updateScore} playBeep={playBeep} />
+        <Semaforo2Semaforo fr="q3/after" to="q4"/>
+        <PauseAt time="q4" />
+        <Question lang={lang} number="4" updateFx={updateScore} playBeep={playBeep} />
+        <Semaforo2Semaforo fr="q4/after" to="q5"/>
+        <PauseAt time="q5" />
+        <Question lang={lang} number="5" updateFx={updateScore} playBeep={playBeep} />
+        <Semaforo2Semaforo fr="q5/after" to="q6"/>
+        <PauseAt time="q6" />
+        <Question lang={lang} number="6" updateFx={updateScore} playBeep={playBeep} />
+        <Semaforo2Semaforo fr="q6/after" to="q7"/>
+        <PauseAt time="q7" />
+        <Question lang={lang} number="7" updateFx={updateScore} playBeep={playBeep} />
+        <Semaforo2Semaforo fr="q7/after" to="q8"/>
+        <PauseAt time="q8" />
+        <Question lang={lang} number="8" updateFx={updateScore} playBeep={playBeep} />
+        <Semaforo2Semaforo fr="q8/after" to="q9"/>
+        <PauseAt time="q9" />
+        <Question lang={lang} number="9" updateFx={updateScore} playBeep={playBeep} />
+        <Semaforo2Semaforo fr="q9/after" to="q10"/>
+        <PauseAt time="q10" />
+        <Question lang={lang} number="10" updateFx={updateScore} playBeep={playBeep} />
+        <Arrivo/>
+        <PauseAt time="response" />
+        <Response lang={lang} score={score} updateFx={updateScore} qr={QRs} />
+        <Fail />
+        <PauseAt time="retry" />
+        <Retry lang={lang} updateFx={updateScore} />
+        <PauseAt time="credits" />
+        <Credits lang={lang} />
+      </Player>
+      <Lines />
+    </Fragment>
   );
 }
 // ArrowLeft, ArrowRight
